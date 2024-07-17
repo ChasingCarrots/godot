eed0ba0142451722d64fe8696a9f9c05bbedf890
Fabio Mangiameli <fabio.mangiameli22@gmail.com>
2024-07-17 12:01:06 +0200

    Revert "Threaded Object pool now works with ThreadWorker"
    This reverts commit 24c04ec2c4fc6ded21477099fcf9310ac53c5949.

--- modules/HOT/ThreadedObjectPool.h
+++ modules/HOT/ThreadedObjectPool.h
@@ -33,12 +33,12 @@ protected:
 		Callable CreationCallback;
 	};
 	LocalVector<InstanceCreationData> _instanceCreationQueue;
-	LocalVector<InstanceCreationData> _finishedCreations;
-	LocalVector<WorkerThreadPool::TaskID> _taskIDsInQueue;
-
 	Mutex _instanceListMutex;
+	Semaphore _instanceCreationSemaphore;
+	bool _endThread = false;
 	bool _is_clearing_instances = false;
-	void handleCreationQueueElement();
+	Thread _instanceCreationThread;
+	static void instance_creation_thread_loop(void *p_ud);
 
 public:
 	~ThreadedObjectPool() override;
