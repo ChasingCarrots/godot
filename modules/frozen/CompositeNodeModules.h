331b9eb9afde073f4ef2c072a628deedd624fca6
Dominik <mail.dominik.s@googlemail.com>
2025-03-11 16:58:38 +0100

    Implemented CompositeNodeModule classes

---
+++ modules/frozen/CompositeNodeModules.h
@@ -0,0 +1,223 @@
+#ifndef COMPOSITENODEMODULES_H
+#define COMPOSITENODEMODULES_H
+
+#include "CompositeNode.h"
+#include "CompositeNodeValue.h"
+
+class CompositeNodeModule : public Node {
+	GDCLASS(CompositeNodeModule, Node)
+protected:
+	// Required entry point that the API calls to bind our class to Godot.
+	static void _bind_methods() {
+		ClassDB::bind_method(D_METHOD("get_composite_node"),
+				&CompositeNodeModule::get_composite_node);
+		ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "_composite_node", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_NONE),
+				"", "get_composite_node");
+
+		ClassDB::bind_method(D_METHOD("create_non_synchronized_value", "value_name", "initial_value"),
+				&CompositeNodeModule::create_non_synchronized_value);
+		ClassDB::bind_method(D_METHOD("create_synchronized_value", "value_name", "initial_value", "sync_mode", "sync_type"),
+				&CompositeNodeModule::create_synchronized_value);
+
+		ClassDB::bind_method(D_METHOD("register_callback", "callback_name", "callable"),
+			&CompositeNodeModule::register_callback);
+		ClassDB::bind_method(D_METHOD("register_function", "function_name", "callable"),
+			&CompositeNodeModule::register_function);
+		ClassDB::bind_method(D_METHOD("register_data_updated_callback", "data_name", "callable"),
+			&CompositeNodeModule::register_data_updated_callback);
+
+		GDVIRTUAL_BIND(_ready_composite_node);
+		GDVIRTUAL_BIND(_ready_authority);
+	}
+
+	void _notification(int p_notification) {
+		switch (p_notification) {
+			case NOTIFICATION_READY:
+				if (!Engine::get_singleton()->is_editor_hint()) {
+					_ready();
+				}
+				break;
+			case NOTIFICATION_EXIT_TREE:
+				if (!Engine::get_singleton()->is_editor_hint()) {
+					_exit_tree();
+				}
+				break;
+		}
+	}
+	void _ready() {
+		_composite_node = CompositeNode::GetCompositeNodeInParents(this);
+		ERR_FAIL_COND_MSG(!_composite_node.is_valid(), vformat("CompositeNodeModule has to be a child of a CompositeNode. (%s)", get_name()));
+		_composite_node->RegisterCallback("init_authority", callable_mp(this, &CompositeNodeModule::init_authority));
+
+		GDVIRTUAL_CALL(_ready_composite_node);
+	}
+	void _exit_tree() {
+		if (_composite_node.is_valid()) {
+			_composite_node->UnregisterCallback("init_authority", callable_mp(this, &CompositeNodeModule::init_authority));
+			for (const auto &named_callable : RegisteredCallbacks) {
+				_composite_node->UnregisterCallback(named_callable.Name, named_callable.CallableObj);
+			}
+			for (const auto &named_callable : RegisteredFunctions) {
+				_composite_node->UnregisterFunction(named_callable.Name, named_callable.CallableObj);
+			}
+			for (const auto &named_callable : RegisteredDataUpdates) {
+				_composite_node->UnregisterDataUpdatedCallback(named_callable.Name, named_callable.CallableObj);
+			}
+		}
+	}
+
+	SafeObjectPointer<CompositeNode> _composite_node;
+
+	struct NamedCallable {
+		StringName Name;
+		Callable CallableObj;
+	};
+	Vector<NamedCallable> RegisteredCallbacks;
+	Vector<NamedCallable> RegisteredFunctions;
+	Vector<NamedCallable> RegisteredDataUpdates;
+
+	void init_authority(int player_id) {
+		GDVIRTUAL_CALL(_ready_authority);
+	}
+
+public:
+	GDVIRTUAL0(_ready_composite_node)
+	GDVIRTUAL0(_ready_authority)
+
+	Ref<CompositeNodeValue> create_non_synchronized_value(StringName value_name, Variant initial_value) {
+		ERR_FAIL_COND_V_MSG(!_composite_node.is_valid(), {}, "CompositeNodeModule::create_non_synchronized_value called with no composite node set. Called too early? Use the _ready_composite_node virtual function.");
+		return CompositeNodeValue::create_non_synchronized(_composite_node.get_nocheck(), value_name, initial_value);
+	}
+	Ref<CompositeNodeValue> create_synchronized_value(StringName value_name, Variant initial_value,
+			CompositeNode::DataSynchronizationMode mode,
+			CompositeNode::DataSynchronizationType type) {
+		ERR_FAIL_COND_V_MSG(!_composite_node.is_valid(), {}, "CompositeNodeModule::create_synchronized_value called with no composite node set. Called too early? Use the _ready_composite_node virtual function.");
+		return CompositeNodeValue::create_synchronized(_composite_node.get_nocheck(), value_name, initial_value, mode, type);
+	}
+	void register_callback(StringName callback_name, Callable callable) {
+		ERR_FAIL_COND_MSG(!_composite_node.is_valid(), "CompositeNodeModule::register_callback called with no composite node set. Called too early? Use the _ready_composite_node virtual function.");
+		RegisteredCallbacks.append({ callback_name, callable });
+		_composite_node->RegisterCallback(callback_name, callable);
+	}
+	void register_function(StringName function_name, Callable callable) {
+		ERR_FAIL_COND_MSG(!_composite_node.is_valid(), "CompositeNodeModule::register_function called with no composite node set. Called too early? Use the _ready_composite_node virtual function.");
+		RegisteredFunctions.append({ function_name, callable });
+		_composite_node->RegisterFunction(function_name, callable);
+	}
+	void register_data_updated_callback(StringName data_name, Callable callable) {
+		ERR_FAIL_COND_MSG(!_composite_node.is_valid(), "CompositeNodeModule::register_data_updated_callback called with no composite node set. Called too early? Use the _ready_composite_node virtual function.");
+		RegisteredDataUpdates.append({ data_name, callable });
+		_composite_node->RegisterDataUpdatedCallback(data_name, callable);
+	}
+	[[nodiscard]] CompositeNode *get_composite_node() const { return _composite_node.get_nocheck(); }
+};
+
+class CompositeNodeModule3D : public Node3D {
+	GDCLASS(CompositeNodeModule3D, Node3D)
+protected:
+	// Required entry point that the API calls to bind our class to Godot.
+	static void _bind_methods() {
+		ClassDB::bind_method(D_METHOD("get_composite_node"),
+				&CompositeNodeModule3D::get_composite_node);
+		ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "_composite_node", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_NONE),
+				"", "get_composite_node");
+
+		ClassDB::bind_method(D_METHOD("create_non_synchronized_value", "value_name", "initial_value"),
+				&CompositeNodeModule3D::create_non_synchronized_value);
+		ClassDB::bind_method(D_METHOD("create_synchronized_value", "value_name", "initial_value", "sync_mode", "sync_type"),
+				&CompositeNodeModule3D::create_synchronized_value);
+
+		ClassDB::bind_method(D_METHOD("register_callback", "callback_name", "callable"),
+			&CompositeNodeModule3D::register_callback);
+		ClassDB::bind_method(D_METHOD("register_function", "function_name", "callable"),
+			&CompositeNodeModule3D::register_function);
+		ClassDB::bind_method(D_METHOD("register_data_updated_callback", "data_name", "callable"),
+			&CompositeNodeModule3D::register_data_updated_callback);
+
+		GDVIRTUAL_BIND(_ready_composite_node);
+		GDVIRTUAL_BIND(_ready_authority);
+	}
+
+	void _notification(int p_notification) {
+		switch (p_notification) {
+			case NOTIFICATION_READY:
+				if (!Engine::get_singleton()->is_editor_hint()) {
+					_ready();
+				}
+				break;
+			case NOTIFICATION_EXIT_TREE:
+				if (!Engine::get_singleton()->is_editor_hint()) {
+					_exit_tree();
+				}
+				break;
+		}
+	}
+	void _ready() {
+		_composite_node = CompositeNode::GetCompositeNodeInParents(this);
+		ERR_FAIL_COND_MSG(!_composite_node.is_valid(), vformat("CompositeNodeModule3D has to be a child of a CompositeNode. (%s)", get_name()));
+		_composite_node->RegisterCallback("init_authority", callable_mp(this, &CompositeNodeModule3D::init_authority));
+
+		GDVIRTUAL_CALL(_ready_composite_node);
+	}
+	void _exit_tree() {
+		if (_composite_node.is_valid()) {
+			_composite_node->UnregisterCallback("init_authority", callable_mp(this, &CompositeNodeModule3D::init_authority));
+			for (const auto &named_callable : RegisteredCallbacks) {
+				_composite_node->UnregisterCallback(named_callable.Name, named_callable.CallableObj);
+			}
+			for (const auto &named_callable : RegisteredFunctions) {
+				_composite_node->UnregisterFunction(named_callable.Name, named_callable.CallableObj);
+			}
+			for (const auto &named_callable : RegisteredDataUpdates) {
+				_composite_node->UnregisterDataUpdatedCallback(named_callable.Name, named_callable.CallableObj);
+			}
+		}
+	}
+
+	SafeObjectPointer<CompositeNode> _composite_node;
+
+	struct NamedCallable {
+		StringName Name;
+		Callable CallableObj;
+	};
+	Vector<NamedCallable> RegisteredCallbacks;
+	Vector<NamedCallable> RegisteredFunctions;
+	Vector<NamedCallable> RegisteredDataUpdates;
+
+	void init_authority(int player_id) {
+		GDVIRTUAL_CALL(_ready_authority);
+	}
+
+public:
+	GDVIRTUAL0(_ready_composite_node)
+	GDVIRTUAL0(_ready_authority)
+
+	Ref<CompositeNodeValue> create_non_synchronized_value(StringName value_name, Variant initial_value) {
+		ERR_FAIL_COND_V_MSG(!_composite_node.is_valid(), {}, "CompositeNodeModule3D::create_non_synchronized_value called with no composite node set. Called too early? Use the _ready_composite_node virtual function.");
+		return CompositeNodeValue::create_non_synchronized(_composite_node.get_nocheck(), value_name, initial_value);
+	}
+	Ref<CompositeNodeValue> create_synchronized_value(StringName value_name, Variant initial_value,
+			CompositeNode::DataSynchronizationMode mode,
+			CompositeNode::DataSynchronizationType type) {
+		ERR_FAIL_COND_V_MSG(!_composite_node.is_valid(), {}, "CompositeNodeModule3D::create_synchronized_value called with no composite node set. Called too early? Use the _ready_composite_node virtual function.");
+		return CompositeNodeValue::create_synchronized(_composite_node.get_nocheck(), value_name, initial_value, mode, type);
+	}
+	void register_callback(StringName callback_name, Callable callable) {
+		ERR_FAIL_COND_MSG(!_composite_node.is_valid(), "CompositeNodeModule3D::register_callback called with no composite node set. Called too early? Use the _ready_composite_node virtual function.");
+		RegisteredCallbacks.append({ callback_name, callable });
+		_composite_node->RegisterCallback(callback_name, callable);
+	}
+	void register_function(StringName function_name, Callable callable) {
+		ERR_FAIL_COND_MSG(!_composite_node.is_valid(), "CompositeNodeModule3D::register_function called with no composite node set. Called too early? Use the _ready_composite_node virtual function.");
+		RegisteredFunctions.append({ function_name, callable });
+		_composite_node->RegisterFunction(function_name, callable);
+	}
+	void register_data_updated_callback(StringName data_name, Callable callable) {
+		ERR_FAIL_COND_MSG(!_composite_node.is_valid(), "CompositeNodeModule3D::register_data_updated_callback called with no composite node set. Called too early? Use the _ready_composite_node virtual function.");
+		RegisteredDataUpdates.append({ data_name, callable });
+		_composite_node->RegisterDataUpdatedCallback(data_name, callable);
+	}
+	[[nodiscard]] CompositeNode *get_composite_node() const { return _composite_node.get_nocheck(); }
+};
+
+#endif //COMPOSITENODEMODULES_H
--- modules/frozen/register_types.cpp
+++ modules/frozen/register_types.cpp
@@ -5,6 +5,7 @@
 #include "FutureValue.h"
 #include "CompositeNode.h"
 #include "CompositeNodeValue.h"
+#include "CompositeNodeModules.h"
 
 
 void initialize_frozen_module(ModuleInitializationLevel p_level) {
@@ -16,6 +17,8 @@ void initialize_frozen_module(ModuleInitializationLevel p_level) {
 	ClassDB::register_class<CommunicationCallWithAnswer>();
 	ClassDB::register_class<FutureValue>();
 	ClassDB::register_class<CompositeNode>();
+	ClassDB::register_class<CompositeNodeModule>();
+	ClassDB::register_class<CompositeNodeModule3D>();
 	ClassDB::register_class<CompositeNodeValue>();
 }
 
